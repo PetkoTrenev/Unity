0cb2d8b10b2da551991c3db2d99f4affcd054845
Petko <petkotrenev@gmail.com>
2017-03-25 23:21:26 +0200

    Made a simple game using a ball to move around and collect cubes.

---
+++ Roll-a-ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position; // calucates the distance of the camera to the object
+
+	}
+	
+	// Update is called once per frame, but after all objects have been processed in Update()
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int count;
+
+	public float speed; // this variable is in the inspector
+	public Text countText;
+	public Text winText;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	// before physics calculations
+	// everything with physics goes in here
+	void FixedUpdate() 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");	
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.CompareTag("Pick Up")) 
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 6) {
+			winText.text = "You win!";
+		}
+	}
+
+}
